722dae84ccb2c7200ce82da9dcca3564f4e21ae9
damaainan <damaainan@163.com>
2018-01-11 17:21:07 +0800

    过不去
    过不去 思念谁

--- apps/build.php
+++ apps/build.php
@@ -12,16 +12,24 @@
 // 创建目录的方法 php think build
 return [
     // 生成应用公共文件
-    '__file__' => ['common.php','tags.php'],
+    // '__file__' => ['common.php','tags.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'index'     => [
-        '__file__'   => ['common.php','tags.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
-        'view'       => ['index/index'],
-    ],
+    // 'index'     => [
+    //     '__file__'   => ['common.php','tags.php'],
+    //     '__dir__'    => ['behavior', 'controller', 'model', 'view'],
+    //     'controller' => ['Index', 'Test', 'UserType'],
+    //     'model'      => ['User', 'UserType'],
+    //     'view'       => ['index/index'],
+    // ],
 
     // 其他更多的模块定义
+    'api'     => [
+        '__file__'   => ['common.php','tags.php'],
+        '__dir__'    => ['behavior', 'controller', 'service', 'model', 'view'],
+        'controller' => ['Index', 'Test', 'UserType'],
+        'service'    => ['Index', 'Test', 'UserType'],
+        'model'      => ['User', 'UserType'],
+        // 'view'       => ['index/index'],
+    ],
 ];
